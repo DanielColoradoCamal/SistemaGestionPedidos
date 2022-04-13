11b8612023e2ded577026e8708130ec49fb2b4db
daniel Colorado <daniel.colorado.940426@gmail.com>
2022-04-12 23:33:20 -0500

    Se da formato a formulario

--- contacto/forms.py
+++ contacto/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class ContactoForm(forms.Form):
     nombre = forms.CharField(label='Nombre', max_length=100, required=True)
-    correo = forms.CharField(label='Correo electronico', max_length=100, required=True)
-    mensaje = forms.CharField(label='Mensaje', max_length=500)
+    correo = forms.CharField(label='Correo', max_length=100, required=True)
+    mensaje = forms.CharField(label='Mensaje', max_length=500, widget=forms.Textarea)
