e93044f495549661c970ce44930502c2f46e9029
daniel Colorado <daniel.colorado.940426@gmail.com>
2022-04-15 19:51:37 -0500

    Se genera carrito y widget de carrito

--- proyectoWeb/settings.py
+++ proyectoWeb/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'blog',
     'contacto',
     'tienda',
+    'carro',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carro.context_processor.importe_total_carro',
             ],
         },
     },
