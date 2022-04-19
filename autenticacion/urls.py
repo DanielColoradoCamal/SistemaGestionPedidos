7d39dc6cd3d77c945160cead4010c6f3a5aaf1c3
daniel Colorado <daniel.colorado.940426@gmail.com>
2022-04-19 02:04:13 -0500

    Se crean enlace para registro y login. Punto seguro

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import VistaRegistro
+from .views import VistaRegistro, cerrar_sesion, loguear
 
 
 
 urlpatterns = [
     path('', VistaRegistro.as_view(), name='Autenticacion'),
+    path('cerrar_sesion', cerrar_sesion, name='cerrar_sesion'),
+    path('loguear', loguear, name='loguear'),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 # def autenticacion(request):
@@ -14,14 +15,38 @@ class VistaRegistro(View):
     def post(self,request):
         form= UserCreationForm(request.POST)
 
-        if form.is_valid:   
+        if form.is_valid():   
             usuario=form.save()
             login(request,usuario)
 
             return redirect('Inicio')
         else:
-            pass
+            for mensaje in form.error_messages:
+                messages.error(request, form.error_messages[mensaje])
+            return render(request,'registro/registro.html', {'form':form})
 
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('Inicio')
+
+def loguear(request):
+    if request.method=='POST':   
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario=form.cleaned_data.get('username')
+            contra=form.cleaned_data.get('password')
+            usuario=authenticate(username=nombre_usuario,password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('Inicio')
+            else:
+                messages.error(request, 'Usuario no valido')
+        else:
+            messages.error(request, 'Usuario no valido')
+
+    form=AuthenticationForm()
+    return render(request,'login/login.html', {'form':form})
+    
 
 
         
